86260efa846716de00040f47ee4a9084e0e00048
Mynameisrohith <110761054+Mynameisrohith@users.noreply.github.com>
2026-01-17 15:37:40 +0530

    ListDemoExample

---
+++ demoproject/src/main/java/gentech/com/Collections/ListDemo.java
@@ -0,0 +1,18 @@
+package gentech.com.Collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListDemo {
+
+    public static void main(String[] args) {
+        List<String> l=new ArrayList<>();
+        String str[]=new String[]{"Rohith","Bharath","Mohan","mama"};
+        for(String i:str){
+            l.add(i);
+        }
+        System.out.println(l);
+
+
+    }
+}
