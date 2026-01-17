b211d56c0eb43d5f1be3c423c53b385c91899311
Mynameisrohith <110761054+Mynameisrohith@users.noreply.github.com>
2026-01-17 14:09:01 +0530

    reverse the array

---
+++ demoproject/src/main/java/gentech/com/programs/ReverseArray.java
@@ -0,0 +1,11 @@
+package gentech.com.programs;
+
+public class ReverseArray {
+
+    public static void main(String[] args) {
+        int a[]={1,2,3,4,5,6,7,8,9,10};
+        for(int i=a.length;i>=1;i--){
+            System.out.println(i);
+        }
+    }
+}
